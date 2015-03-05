f9e294def7820e94b1af5599d486828133a39545
William <william.flaherty@gmail.com>
2015-03-04 18:31:30 -0600

    For some reason this file didn't commit :|

---
+++ Pearing App/PEConfiguration.h
@@ -0,0 +1,23 @@
+//
+//  PEConfiguration.h
+//  Pearing
+//
+//  Created by Nathan Ziebart on 2/22/15.
+//  Copyright (c) 2015 Pearing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+
+// Server
+extern NSString *const PEConfiguration_ServerURL;
+extern NSString *const PEConfiguration_UpdateUserURL;
+extern NSString *const PEConfiguration_RegisterUserURL;
+
+// Instagram
+extern NSString *const PEConfiguration_InstagramRedirectURL;
+extern NSString *const PEConfiguration_InstagramClientID;
+extern NSString *const PEConfiguration_InstagramClientSecret;
+extern NSString *const PEConfiguration_InstagramAuthURL;
+extern NSString *const PEConfiguration_InstagramAPIURL;
